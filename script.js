8762681f07e642d854825fd8c53adb521a5149fa
jeevaphysicist <jeevaphysicist@gmail.com>
2022-09-27 19:49:09 +0530

    submisson

---
+++ script.js
@@ -0,0 +1,9 @@
+var options = {
+    strings: [" Jeevanantham S . "," a Full Stack Developer . "," a Front-end Developer . "," a Back-end Developer . "],
+    typeSpeed: 60,
+    backSpeed:70,
+    loop:true,
+
+  };
+  
+  var typed = new Typed('.value', options);
